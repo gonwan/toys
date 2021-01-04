97f6ff353564f766f3e7a95b7f8e463be6267488
gonwan <gonwan@gmail.com>
2021-01-04 18:59:20 +0800

    add json converter.

--- kafka-tx/src/main/java/com/gonwan/kafka/tx/KafkaTransactionApplication.java
+++ kafka-tx/src/main/java/com/gonwan/kafka/tx/KafkaTransactionApplication.java
@@ -12,10 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultAfterRollbackProcessor;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.transaction.ChainedKafkaTransactionManager;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -40,18 +43,24 @@ public class KafkaTransactionApplication {
         return new ChainedKafkaTransactionManager<>(kafkaTransactionManager, transactionManager);
     }
 
+    @Bean
+    public DeadLetterPublishingRecoverer deadLetterPublishingRecoverer(KafkaOperations<?, ?> kafkaOperations) {
+        return new DeadLetterPublishingRecoverer(kafkaOperations);
+    }
+
     @SuppressWarnings("unchecked")
     @Bean
     public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaTransactionListenerContainerFactory(
             ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
             ConsumerFactory<?, ?> consumerFactory,
+            DeadLetterPublishingRecoverer deadLetterPublishingRecoverer,
             ChainedKafkaTransactionManager<?, ?> chainedKafkaTransactionManager) {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, (ConsumerFactory<Object, Object>) consumerFactory);
         /* transaction id defaults '<transactionIdPrefix>.<group.id>.<topic>.<partition>' */
         factory.getContainerProperties().setTransactionManager(chainedKafkaTransactionManager); /* transaction support */
         factory.setMessageConverter(new JsonMessageConverter()); /* json support */
-        factory.setAfterRollbackProcessor(new DefaultAfterRollbackProcessor<>(new FixedBackOff(0L, 2)));
+        factory.setAfterRollbackProcessor(new DefaultAfterRollbackProcessor<>(deadLetterPublishingRecoverer, new FixedBackOff(0L, 2)));
         return factory;
     }
 
@@ -59,7 +68,9 @@ public class KafkaTransactionApplication {
     @Bean
     @Primary
     public KafkaTemplate<?, ?> kafkaTemplate(ProducerFactory<?, ?> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+        KafkaTemplate<?, ?> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
+        return kafkaTemplate;
     }
 
     /* Used in consumer-initiated transactions */
@@ -77,6 +88,7 @@ public class KafkaTransactionApplication {
     @Bean("standaloneKafkaTemplate")
     public KafkaTemplate<?, ?> standaloneKafkaTemplate(KafkaProperties kafkaProperties, ProducerFactory<?, ?> producerFactory) {
         KafkaTemplate<?, ?> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
         /* transaction id defaults to 'transactionIdPrefix + n', override it. */
         kafkaTemplate.setTransactionIdPrefix(String.format("%s%s-",
                 kafkaProperties.getProducer().getTransactionIdPrefix(), UUID.randomUUID().toString()));
--- kafka-tx/src/main/java/com/gonwan/kafka/tx/controller/KafkaController.java
+++ kafka-tx/src/main/java/com/gonwan/kafka/tx/controller/KafkaController.java
@@ -1,8 +1,15 @@
 package com.gonwan.kafka.tx.controller;
 
+import com.gonwan.kafka.tx.config.Config;
+import com.gonwan.kafka.tx.model.User;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +21,27 @@ import java.util.Collections;
 public class KafkaController {
 
     @Autowired
+    private Config config;
+
+    @Autowired
     private KafkaAdmin kafkaAdmin;
+
+    @Autowired
+    @Qualifier("standaloneKafkaTemplate")
+    private KafkaTemplate<?, ?> kafkaTemplate;
+
+    @GetMapping("/topic/send")
+    public void topicSend(String message) {
+        User user = new User();
+        user.setId(123456L);
+        user.setUsername("hahausername");
+        user.setPassword("hahapassword");
+        Message<User> msg = MessageBuilder
+                .withPayload(user)
+                .setHeader(KafkaHeaders.TOPIC, config.getKafkaTestTopic())
+                .build();
+        kafkaTemplate.send(msg);
+    }
 
     @GetMapping("/topic/delete")
     public void topicDelete(String topic) {
