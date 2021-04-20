cfbe5f3c16a1f7b225291c598de35946c671195e
Binhao Qian <gonwan@gmail.com>
2021-04-21 00:52:49 +0800

    add grpc.

---
+++ grpc-snippet/grpc-server/src/main/java/com/gonwan/snippet/grpc/GrpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.gonwan.snippet.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+
+}
