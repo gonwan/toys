772b1968fa51fc3ee399479456954b2cfe0b349a
Binhao Qian <qianbinhao@eastmoney.com>
2022-07-14 18:18:05 +0800

    fix typos.

--- consistent-cache/src/main/resources/user.sql
+++ consistent-cache/src/main/resources/user.sql
@@ -4,7 +4,7 @@ CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `version` bigint(20) DEFAULT NULL
+  `version` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
