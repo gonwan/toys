d291e6ca02114859dc4d61074a16c337bc4895f5
Binhao Qian <qianbinhao@eastmoney.com>
2022-07-14 20:45:24 +0800

    add back comments.

--- consistent-cache/src/main/java/com/gonwan/toys/consistentcache/service/Cache2Service.java
+++ consistent-cache/src/main/java/com/gonwan/toys/consistentcache/service/Cache2Service.java
@@ -23,11 +23,37 @@ public class Cache2Service {
 
     public void saveUser(UserDO userDO) {
         userRepository.save(userDO);
+        /*
+         * This example only shows the usage of rockscache-java. Therefore,
+         * it directly calls the tagAsDeleted method after modifying the database
+         * and does not consider that the tagAsDeleted method itself may throw exception.
+         *
+         * In actual projects, please ensure that tagAsDeleted will be executed successfully.
+         * It is recommended to use reliable messages.
+         *
+         * For example, you can use the 2-phase message of the DTM framework to ensure that
+         * tagAsDeleted will be retried and retried until it succeeds.
+         *
+         * Please see https://en.dtm.pub/practice/msg.html to know more
+         */
         userCache.tagAsDeleted(userDO.getId());
     }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
+        /*
+         * This example only shows the usage of rockscache-java. Therefore,
+         * it directly calls the tagAsDeleted method after modifying the database
+         * and does not consider that the tagAsDeleted method itself may throw exception.
+         *
+         * In actual projects, please ensure that tagAsDeleted will be executed successfully.
+         * It is recommended to use reliable messages.
+         *
+         * For example, you can use the 2-phase message of the DTM framework to ensure that
+         * tagAsDeleted will be retried and retried until it succeeds.
+         *
+         * Please see https://en.dtm.pub/practice/msg.html to know more
+         */
         userCache.tagAsDeleted(id);
     }
 
