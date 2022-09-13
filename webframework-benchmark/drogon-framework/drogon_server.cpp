8911597538440f690ea8985a6960f7e8dd66a1ba
Binhao Qian <qianbinhao@eastmoney.com>
2022-09-13 20:47:27 +0800

    add drogon-framework.

---
+++ webframework-benchmark/drogon-framework/drogon_server.cpp
@@ -0,0 +1,32 @@
+// g++ -O2 drogon_server.cpp -o drogon_server -I/usr/include/boost169 -ldrogon -ltrantor -lcrypto -lssl -luuid -ldl -lz -lpthread /usr/lib64/libboost_filesystem.so.1.69.0 /usr/local/lib64/libjsoncpp.a
+// ~34w/s, slightly slow than go-gin server...
+#include <drogon/drogon.h>
+using namespace drogon;
+
+int main()
+{
+    app().registerHandler(
+        "/text",
+        [](const HttpRequestPtr &,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            auto resp = HttpResponse::newHttpResponse();
+            resp->setBody("Hello World!");
+            callback(resp);
+        },
+        {Get});
+    app().registerHandler(
+        "/json",
+        [](const HttpRequestPtr &,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            Json::Value json;
+            json["Message"] = "Hello World!";
+            auto resp = HttpResponse::newHttpJsonResponse(json);
+            callback(resp);
+        },
+        {Get});
+    LOG_INFO << "Server running on 0.0.0.0:8099";
+    app().addListener("0.0.0.0", 8099)
+         .setThreadNum(24)
+         .enableRunAsDaemon()
+         .run();
+}
---
+++ webframework-benchmark/drogon-framework/readme.md
@@ -0,0 +1,30 @@
+## CentOS 7 Build
+- Enable official SCL repository, install g++-7, and install:
+```
+yum install libuuid-devel
+yum install zlib-devel
+yum install openssl-devel
+yum install boost169-devel
+```
+- Install cmake from official `*.tar.gz` file.
+- Build install `jsoncpp`:
+```
+git clone https://github.com/open-source-parsers/jsoncpp
+cd jsoncpp/
+git checkout 1.9.5
+mkdir build
+cd build
+cmake ..
+make && make install
+```
+- Build install `drogon` framework:
+```
+git clone https://github.com/drogonframework/drogon
+cd drogon
+git checkout 1.8.0
+git submodule update --init
+mkdir build
+cd build
+cmake .. -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169
+make && sudo make install
+```
