687b73e0828396478c91d2ab767bd4a883b0cf38
Binhao Qian <qianbinhao@eastmoney.com>
2023-01-04 17:06:23 +0800

    add sse test.

--- webframework-benchmark/drogon/drogon_server.cpp
+++ webframework-benchmark/drogon/drogon_server.cpp
@@ -1,10 +1,16 @@
 // g++ -O2 drogon_server.cpp -o drogon_server -I/usr/include/boost169 -ldrogon -ltrantor -lcrypto -lssl -luuid -ldl -lz -lpthread /usr/lib64/libboost_filesystem.so.1.69.0 /usr/local/lib64/libjsoncpp.a
-// ~33w/s, slightly slow than go-gin server. No difference after disabling server & date headers...
+// ~33w/s, slightly slower than go-gin server. No difference after disabling server & date headers...
+#include <thread>
 #include <drogon/drogon.h>
+using namespace std;
 using namespace drogon;
 
 int main()
 {
+    app().registerPostHandlingAdvice(
+        [](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
+            resp->addHeader("Access-Control-Allow-Origin", "*");
+        });
     app().registerHandler(
         "/text",
         [](const HttpRequestPtr &,
@@ -24,11 +30,25 @@ int main()
             callback(resp);
         },
         {Get});
+    app().registerHandler(
+        "/sse",
+        [](const HttpRequestPtr &,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            int i = 0;
+            auto cb = [&i](char *buffer, size_t size) -> size_t {
+                this_thread::sleep_for(1s);
+                int c = std::snprintf(buffer, size, "id: %d\ndata: datatata\n\n", ++i);
+                return c;
+            };
+            auto resp = drogon::HttpResponse::newStreamResponse(cb, "", CT_CUSTOM, "text/event-stream");
+            callback(resp);
+        },
+        {Get});
     LOG_INFO << "Server running on 0.0.0.0:8099";
     app().addListener("0.0.0.0", 8099)
-         .setThreadNum(24)
-         .enableServerHeader(false)
-         .enableDateHeader(false)
-         .enableRunAsDaemon()
-         .run();
+        .setThreadNum(24)
+        .enableServerHeader(false)
+        .enableDateHeader(false)
+        .enableRunAsDaemon()
+        .run();
 }
