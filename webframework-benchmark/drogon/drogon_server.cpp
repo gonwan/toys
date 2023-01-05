beedc736e399a6151bf775456170652babd92daf
Binhao Qian <qianbinhao@eastmoney.com>
2023-01-05 23:27:12 +0800

    add thread pool.

--- webframework-benchmark/drogon/drogon_server.cpp
+++ webframework-benchmark/drogon/drogon_server.cpp
@@ -1,12 +1,14 @@
 // g++ -O2 drogon_server.cpp -o drogon_server -I/usr/include/boost169 -ldrogon -ltrantor -lcrypto -lssl -luuid -ldl -lz -lpthread /usr/lib64/libboost_filesystem.so.1.69.0 /usr/local/lib64/libjsoncpp.a
 // ~33w/s, slightly slower than go-gin server. No difference after disabling server & date headers...
 #include <thread>
+#include <boost/asio.hpp>
 #include <drogon/drogon.h>
 using namespace std;
 using namespace drogon;
 
 int main()
 {
+    boost::asio::thread_pool pool(200);
     app().registerPostHandlingAdvice(
         [](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
             resp->addHeader("Access-Control-Allow-Origin", "*");
@@ -44,11 +46,26 @@ int main()
             callback(resp);
         },
         {Get});
+    app().registerHandler(
+        "/longtask",
+        [&pool](const HttpRequestPtr &req,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            /* pass in req to extend request lifecycle */
+            boost::asio::post(pool, [req, callback](){
+                this_thread::sleep_for(10s);
+                Json::Value json;
+                json["Message"] = "Hello, World!";
+                auto resp = HttpResponse::newHttpJsonResponse(json);
+                callback(resp);
+            });
+        },
+        {Get});
     LOG_INFO << "Server running on 0.0.0.0:8099";
     app().addListener("0.0.0.0", 8099)
-        .setThreadNum(24)
+        .setThreadNum(0)
+        .setMaxConnectionNum(10000)
         .enableServerHeader(false)
         .enableDateHeader(false)
-        .enableRunAsDaemon()
+        //.enableRunAsDaemon()
         .run();
 }
